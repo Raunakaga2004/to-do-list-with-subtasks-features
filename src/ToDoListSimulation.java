b60f07bdc4bf10c59b7cb4fcb9ca0e04916f4b4d
Raunakaga2004 <raunakaga12@gmail.com>
2024-07-30 23:12:08 +0530

    30/7

--- src/ToDoListSimulation.java
+++ src/ToDoListSimulation.java
@@ -2,29 +2,31 @@ import java.util.Scanner;
 
 public class ToDoListSimulation extends ToDoList{
     ToDoListSimulation() throws Exception {
-        simulate();
+
+        simulate(new ToDoList());
     }
-    public void simulate() throws Exception {
+    public void simulate(ToDoList list) throws Exception {
         System.out.println("Program Started ... ");
 
-        ToDoList list = new ToDoList();
+//        ToDoList list = new ToDoList();
         Scanner sc = new Scanner(System.in);
 
         int taskNum = -1;
 
         while(true){
             System.out.println("\n\nDo you want to :\n" +
-                    "\tadd task - type 'add'\n" +
-                    "\tdelete task - type 'delete'\n" +
+                    "\tadd task - type 'add'\n" +  //done
+                    "\tdelete task - type 'delete'\n" +  //done
                     "\tadd category - type 'add cat'\n" +
                     "\tdelete category - type 'delete cat'\n" +
-                    "\tdisplay tasks and subtasks - type 'display'\n" +
+                    "\tdisplay tasks and subtasks - type 'display'\n" + //done
                     "\tdisplay categories - type 'display cat'\n" +
                     "\tdisplay tasks in detail - type 'display task in detail'\n" +
                     "\tcheck progress - type 'progress'\n" +
                     "\tcategorize task on basis of category - type 'category'\n" +
                     "\tlist of completed or uncompleted tasks - type 'check'\n" +
-                    "Close the program - type 'exit'");
+                    "\tto access task - type 'task'\n" + //done
+                    "Close the program - type 'exit'"); //done
 
             System.out.println("\nType : ");
             String str = sc.nextLine();
@@ -33,14 +35,31 @@ public class ToDoListSimulation extends ToDoList{
             if(str.equals("add")){
                 list.add();
                 taskNum ++;
-                addSubTasks(list, list.headTask.taskName , sc, taskNum);
+                addSubTasks(list, taskNum);
             }
 
             //delete task or subtask
+            if(str.equals("delete")){
+                System.out.println("Enter the number of Task you want to delete : ");
+                int num = sc.nextInt() -1;
+                list.deleteTask(num);
+            }
 
             //display task and subtasks
             if(str.equals("display")){
                 list.displayTasksAndSubTasks();
+            }
+
+            //to access the tasks
+            if(str.equals("task")){
+                System.out.println("Enter the number of task you want to access : ");
+                int num = sc.nextInt();
+                Task node = list.headTask;
+                while(num != 1){
+                    node = node.Next;
+                    num--;
+                }
+                simulate(node.subtasks);
             }
 
             if(str.equals("exit"))break;
@@ -49,14 +68,22 @@ public class ToDoListSimulation extends ToDoList{
         System.out.println("\n\nProgram Ended ... ");
     }
 
-    private void addSubTasks(ToDoList curList, String taskName, Scanner sc, int num) throws Exception {
-        sc = new Scanner(System.in);
+    private void addSubTasks(ToDoList curList, int num) throws Exception {
+        Scanner sc = new Scanner(System.in);
         String addsub = new String("yes");
+        int index = 0;
+
+        Task node = curList.headTask;
+        while(node.Next!=null){
+            node =node.Next;
+        }
         while(addsub.equals("yes")) {
-            System.out.println("Do you want to add Subtasks to '" +taskName + "' (type yes or no) : ");
+            System.out.println("Do you want to add Subtasks to '" +node.taskName + "' (type yes or no) : ");
             addsub = sc.next();
             if(addsub.equals("yes")) {
                 curList.addSubTasks(num);
+                addSubTasks(curList.getSubList(num), index );
+                index++;
             }
         }
     }
